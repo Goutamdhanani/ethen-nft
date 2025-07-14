4873a6ebc34f4cb2dfe51c0b6ea6ea2eea013a55
goutam kumar <106019412+Goutamdhanani@users.noreply.github.com>
2025-07-14 23:54:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	basePath: "",
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-		],
-	},
-	webpack: (config) => {
-		config.externals.push("pino-pretty", "lokijs", "encoding");
-		return config;
-	},
+  reactStrictMode: true,
+  output: 'export',
+  basePath: "/ethen-nft",
+  assetPrefix: "/ethen-nft",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
